f20adae06af8670af887bfbe15883951ef81ac03
Eunju/kiori <eun01300714@gmail.com>
2020-02-17 00:33:04 +0900

    Implement WinMain

--- DirectX_11_Engine/WinMain.cpp
+++ DirectX_11_Engine/WinMain.cpp
@@ -1,4 +1,7 @@
 #include <Windows.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance
 					, _In_opt_ HINSTANCE hPrevInstance
@@ -6,4 +9,5 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance
 					, _In_ int nCmdShow)
 {
 	// ShowWindow();
+	return 0;
 }
