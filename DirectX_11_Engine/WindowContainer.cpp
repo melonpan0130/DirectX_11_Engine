0ebfdeeb8f77f491e4c46da9fac6253925aedc56
Eunju/kiori <eun01300714@gmail.com>
2020-02-19 23:28:18 +0900

    WindowProc 2

--- DirectX_11_Engine/WindowContainer.cpp
+++ DirectX_11_Engine/WindowContainer.cpp
@@ -2,5 +2,6 @@
 
 LRESULT WindowContainer::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	OutputDebugStringA("WINDOW PROC FORM WINDOW CONTAINER\n");
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
