f51a14ad889200b66b664678a985688269da7579
Eunju/kiori <eun01300714@gmail.com>
2020-02-20 23:33:38 +0900

    test mouseEvent

--- DirectX_11_Engine/Engine.cpp
+++ DirectX_11_Engine/Engine.cpp
@@ -50,6 +50,7 @@ void Engine::Update()
 		outmsg += std::to_string(me.GetPosX());
 		outmsg += ", Y: ";
 		outmsg += std::to_string(me.GetPosY());
+		outmsg += "\n";
 		OutputDebugStringA(outmsg.c_str());
 	}
 }
