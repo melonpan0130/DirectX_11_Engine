01226129a745ea559dc6ac64878ba31fc8a79bd7
Eunju/kiori <eun01300714@gmail.com>
2020-02-21 23:21:37 +0900

    mouse messages

--- DirectX_11_Engine/WindowContainer.cpp
+++ DirectX_11_Engine/WindowContainer.cpp
@@ -53,6 +53,48 @@ LRESULT WindowContainer::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM 
 		mouse.OnMouseMove(x, y);
 		return 0;
 	}
+	case WM_LBUTTONDOWN:
+	{
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		mouse.OnLeftPressed(x, y);
+		return 0;
+	}
+	case WM_RBUTTONDOWN:
+	{
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		mouse.OnRightPressed(x, y);
+		return 0;
+	}
+	case WM_MBUTTONDOWN:
+	{
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		mouse.OnMiddlePressed(x, y);
+		return 0;
+	}
+	case WM_LBUTTONUP:
+	{
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		mouse.OnLeftReleased(x, y);
+		return 0;
+	}
+	case WM_RBUTTONUP:
+	{
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		mouse.OnRightReleased(x, y);
+		return 0;
+	}
+	case WM_MBUTTONUP:
+	{
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		mouse.OnMiddleReleased(x, y);
+		return 0;
+	}
 	default:
 		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
