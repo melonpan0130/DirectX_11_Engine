7880bae91ed45c88b421980ca117ab6d39ed4cfc
Eunju/kiori <eun01300714@gmail.com>
2020-02-25 00:46:45 +0900

    Implement Graphics

--- DirectX_11_Engine/Graphics/Graphics.h
+++ DirectX_11_Engine/Graphics/Graphics.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
 class Graphics
 {
+public:
+	bool Initialize(HWND hwnd, int width, int height);
+private:
+	bool InitializeDirectX(HWND hwnd, int width, int height);
 
 };
